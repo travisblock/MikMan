09ad4e892c02760f98bcc0ec125de99ff88539d2
SQL47 <ajidalmajid6@gmail.com>
2020-05-15 23:39:09 +0700

    Redirect

---
+++ app/core/Redirect.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* @author Yusuf Al Majid <ajidalmajid6@gmail.com>
+* @license MIT
+* @version 0.0.1
+*/
+
+class Redirect{
+  public static function to($location = null){
+    if($location){
+      header('Location:' . $location);
+      exit();
+    }
+  }
+}
