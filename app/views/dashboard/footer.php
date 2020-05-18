07d0d825098928ba41963d88c3a760a21394d6b4
SQL47 <ajidalmajid6@gmail.com>
2020-05-18 22:52:02 +0700

    deleted

--- app/views/dashboard/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-</div>
-<script src="<?= BASEURL; ?>public/js/jquery.min.js"></script>
-<script src="<?= BASEURL; ?>public/js/bootstrap.min.js"></script>
-<script src="<?= BASEURL; ?>public/js/script.js"></script>
-</body>
-</html>
