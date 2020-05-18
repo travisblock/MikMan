eb38d6b4f510ed08e5c4cf10ebe2e53636cce965
SQL47 <ajidalmajid6@gmail.com>
2020-05-18 22:53:47 +0700

    add template view

---
+++ app/views/templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard MikMan</title>
+    <link href="<?= BASEURL; ?>public/fonts/font-awesome/css/all.css" rel="stylesheet">
+    <link href="<?= BASEURL; ?>public/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= BASEURL; ?>public/css/style.css">
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <div class="top_navbar">
+        <div class="list-menu">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="top_menu">
+          <div class="logo">MikMan</div>
+          <div class="logout"><a href="<?= BASEURL ?>dashboard/logout">Logout</a></div>
+        </div>
+      </div>
