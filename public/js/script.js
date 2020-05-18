83bb8c863a0db476a4b86cc025880bf4f6fdc782
SQL47 <ajidalmajid6@gmail.com>
2020-05-18 22:55:38 +0700

    update script js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,32 @@
 $(document).ready(function () {
+    var path = window.location.href;
+
+    $('.wrapper .sidebar ul li a').each(function(){
+      //console.log(this);
+      var x = this.nextElementSibling;
+      if(x){
+        console.log(x.firstElementChild.firstElementChild);
+      }
+      if(this.href === path) {
+        $(this).addClass("active");
+      };
+    });
+
+    $('.wrapper .sidebar .submenu li a').each(function(){
+      if(this.className === 'active'){
+        console.log('aktif');
+        //$('.wrapper .sidebar ul li a').addClass('active');
+      }
+    });
+
     $(".wrapper .top_navbar .list-menu").on('click', function() {
       $(".wrapper").toggleClass('collapses');
     });
+
+
+    if (window.matchMedia('(max-width: 800px)').matches){
+      $(".wrapper").addClass('collapses');
+      console.log('lebuoh');
+    }
 
 });
