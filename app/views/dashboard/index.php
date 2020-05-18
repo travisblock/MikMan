551e63a07ad208967a5c1b2fde119a7e92524195
SQL47 <ajidalmajid6@gmail.com>
2020-05-18 22:53:28 +0700

    add dashboard view

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,6 +1,40 @@
 
-      <div class="content-mikman">
-            <h2>Collapsible Sidebar Using Bootstrap 4</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <!-- <div class="content-mikman">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div> -->
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="col-lg-12 bg-light pb-3">
+            <h5 class="text-center p-3">Add router</h5>
+            <form method="POST" action="<?= BASEURL; ?>Router/add">
+              <div class="form-group row">
+                <label class="col-lg-2 col-form-label">IP</label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" name="ip">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-lg-2 col-form-label">User</label>
+                <div class="col-lg-10">
+                  <input type="text" class="form-control" name="user">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-2 col-form-label">Password</label>
+                <div class="col-md-10">
+                  <input type="password" class="form-control" name="pass">
+                </div>
+              </div>
+              <input type="submit" class="btn btn-primary" value="submit">
+            </form>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="col-lg-12 bg-light">
+            <h6 class="text-center">Setting roter</h6>
+
+          </div>
+        </div>
       </div>
---
+++ app/views/dashboard/menu.php
@@ -0,0 +1,60 @@
+      <div class="sidebar">
+        <ul class="list-unstyled components">
+          <li>
+            <a href="<?= BASEURL; ?>dashboard" class="parent">
+              <span class="icon">
+                <i class="fa fa-home"></i>
+              </span>
+              <span class="title">Dashboard</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#" class="parent">
+              <span class="icon">
+                <i class="fa fa-wifi"></i>
+              </span>
+              <span class="title">About</span>
+            </a>
+          </li>
+          <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle parent">
+              <span class="icon"><i class="fa fa-globe"></i></span>
+              <span class="title">Pages</span>
+            </a>
+            <ul class="collapse list-unstyled submenu" id="pageSubmenu">
+                <li>
+                    <a href="<?= BASEURL; ?>dashboard/adduser">
+                      <span class="icon"><i class="fa fa-wifi"></i></span>
+                      <span class="title">Wifi</span>
+                    </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon"><i class="fa fa-globe"></i></span>
+                    <span class="title">Page 2</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon"><i class="fa fa-globe"></i></span>
+                    <span class="title">Page 3</span>
+                  </a>
+                </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="parent">
+              <span class="icon"><i class="fa fa-globe"></i></span>
+              <span class="title">Portofolio</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="parent">
+              <span class="icon"><i class="fa fa-globe"></i></span>
+              <span class="title">Contact</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    <div class="container-mikman">
