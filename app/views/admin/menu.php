0f6f048fb0333b7c5f220387463afcbc4ef97268
SQL47 <ajidalmajid6@gmail.com>
2020-05-19 23:55:04 +0700

    update admin menu

--- app/views/admin/menu.php
+++ app/views/admin/menu.php
@@ -3,56 +3,48 @@
     <li>
       <a href="<?= BASEURL; ?>dashboard" class="parent">
         <span class="icon">
-          <i class="fa fa-home"></i>
+          <i class="fa fa-tachometer-alt"></i>
         </span>
         <span class="title">Dashboard</span>
       </a>
     </li>
 
     <li>
-      <a href="#" class="parent">
-        <span class="icon">
-          <i class="fa fa-wifi"></i>
-        </span>
-        <span class="title">About</span>
-      </a>
-    </li>
-    <li>
       <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle parent">
-        <span class="icon"><i class="fa fa-globe"></i></span>
-        <span class="title">Pages</span>
+        <span class="icon"><i class="fa fa-wifi"></i></span>
+        <span class="title">Hotspot</span>
       </a>
       <ul class="collapse list-unstyled submenu" id="pageSubmenu">
           <li>
               <a href="<?= BASEURL; ?>dashboard/adduser">
-                <span class="icon"><i class="fa fa-wifi"></i></span>
-                <span class="title">Wifi</span>
+                <span class="icon"><i class="fa fa-users"></i></span>
+                <span class="title">Users</span>
               </a>
           </li>
           <li>
             <a href="#">
-              <span class="icon"><i class="fa fa-globe"></i></span>
-              <span class="title">Page 2</span>
+              <span class="icon"><i class="fa fa-users-cog"></i></span>
+              <span class="title">User Profile</span>
             </a>
           </li>
           <li>
             <a href="#">
-              <span class="icon"><i class="fa fa-globe"></i></span>
-              <span class="title">Page 3</span>
+              <span class="icon"><i class="fa fa-wifi"></i></span>
+              <span class="title">Hotspot</span>
             </a>
           </li>
       </ul>
     </li>
     <li>
       <a href="#" class="parent">
-        <span class="icon"><i class="fa fa-globe"></i></span>
-        <span class="title">Portofolio</span>
+        <span class="icon"><i class="fa fa-list"></i></span>
+        <span class="title">Log</span>
       </a>
     </li>
     <li>
       <a href="#" class="parent">
-        <span class="icon"><i class="fa fa-globe"></i></span>
-        <span class="title">Contact</span>
+        <span class="icon"><i class="fa fa-cog"></i></span>
+        <span class="title">System</span>
       </a>
     </li>
   </ul>
