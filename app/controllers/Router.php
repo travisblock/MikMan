5fce6ea0edd421d6d37eebc9c80419b9605922f3
SQL47 <ajidalmajid6@gmail.com>
2020-05-19 23:54:05 +0700

    update router controller

--- app/controllers/Router.php
+++ app/controllers/Router.php
@@ -21,15 +21,28 @@ class Router extends Controller{
     $user = Input::get('user');
     $pass = Input::get('pass');
 
-    // $this->API->debug = TRUE;
     if(!empty($ip) && !empty($user)){
       if($this->API->connect($ip, $user, $pass)){
         Session::set('MikMan', TRUE);
+        Session::set('ip', $ip);
+        Session::set('user', $user);
+        Session::set('pass', $pass);
         Redirect::to('admin');
       }else{
         Redirect::to('dashboard');
       }
-      // var_dump($this->API);
     }
+  }
+
+  public function save(){
+    $file = fopen('app/config/router.php', 'a') or die("Tidak bisa membuka file");
+    $text = "\$data['router'] = array('". $_POST['ip'] ."', '". $_POST['user'] ."', '". $_POST['pass'] ."');" . PHP_EOL;
+    fwrite($file, $text);
+    fclose($file);
+
+    if(is_file('app/config/router.php')){
+      echo "OK";
+    }
+    echo $_POST['ip'];
   }
 }
