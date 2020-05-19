76ff13736a2a59fc27b1e880386466eb5afe913c
SQL47 <ajidalmajid6@gmail.com>
2020-05-19 23:55:12 +0700

    update view dashboard

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -8,26 +8,27 @@
         <div class="col-md-6">
           <div class="col-lg-12 bg-light pb-3">
             <h5 class="text-center p-3">Add router</h5>
-            <form method="POST" action="<?= BASEURL; ?>Router/add">
+            <form method="POST" action="<?= BASEURL; ?>Router/add" id="formRouter">
               <div class="form-group row">
                 <label class="col-lg-2 col-form-label">IP</label>
                 <div class="col-lg-10">
-                  <input type="text" class="form-control" name="ip">
+                  <input type="text" class="form-control" name="ip" id="ip">
                 </div>
               </div>
               <div class="form-group row">
                 <label class="col-lg-2 col-form-label">User</label>
                 <div class="col-lg-10">
-                  <input type="text" class="form-control" name="user">
+                  <input type="text" class="form-control" name="user" id="user">
                 </div>
               </div>
               <div class="form-group row">
                 <label class="col-md-2 col-form-label">Password</label>
                 <div class="col-md-10">
-                  <input type="password" class="form-control" name="pass">
+                  <input type="password" class="form-control" name="pass" id="pass">
                 </div>
               </div>
-              <input type="submit" class="btn btn-primary" value="submit">
+              <button type="button" class="btn btn-success" id="save">Save</button>
+              <button type="submit" class="btn btn-primary">Connect</button>
             </form>
           </div>
         </div>
