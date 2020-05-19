bc1a2c633a52ad7fc7f74bc819506d8a478d3f1c
SQL47 <ajidalmajid6@gmail.com>
2020-05-19 23:53:55 +0700

    update admin

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -9,9 +9,14 @@ class Admin extends Controller{
   }
 
   public function index(){
+    //$this->API->debug = true;
+    $date = $this->API->comm("/system/clock/print");
+    $data['date'] = ucfirst($date[0]['date']);
+    $data['jam']  = $date[0]['time'];
+
     $this->view('templates/header');
     $this->view('admin/menu');
-    $this->view('admin/index');
+    $this->view('admin/index', $data);
     $this->view('templates/footer');
   }
 }
