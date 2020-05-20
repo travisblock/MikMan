b31b7b3c25b5fe825e13f9a4f13c3cfbd1e42331
SQL47 <ajidalmajid6@gmail.com>
2020-05-20 23:32:36 +0700

    update admin controller

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -9,14 +9,37 @@ class Admin extends Controller{
   }
 
   public function index(){
-    //$this->API->debug = true;
-    $date = $this->API->comm("/system/clock/print");
-    $data['date'] = ucfirst($date[0]['date']);
-    $data['jam']  = $date[0]['time'];
-
     $this->view('templates/header');
     $this->view('admin/menu');
-    $this->view('admin/index', $data);
+    $this->view('admin/index');
     $this->view('templates/footer');
   }
+
+  public function infoDashboard(){
+
+    $date = $this->API->comm("/system/clock/print");
+    $data['date'] = ucfirst($date[0]['date']);
+    $data['time']  = $date[0]['time'];
+
+    $resource = $this->API->comm("/system/resource/print");
+    $data['cpu_load'] = $resource[0]['cpu-load'];
+    $data['free_memory'] = $resource[0]['free-memory'];
+    $data['free_hdd'] = $resource[0]['free-hdd-space'];
+    $data['uptime'] = $resource[0]['uptime'];
+    $data['version'] = $resource[0]['version'];
+    $data['board_name'] = $resource[0]['board-name'];
+
+    $model = $this->API->comm("/system/routerboard/print");
+    $data['model'] = ($model[0]['routerboard'] !== 'false') ? $model[0]['routerboard'] : "No";
+
+    echo json_encode($data);
+  }
+
+  public function users(){
+    $this->view('templates/header');
+    $this->view('admin/menu');
+    $this->view('admin/users');
+    $this->view('templates/footer');
+  }
+
 }
