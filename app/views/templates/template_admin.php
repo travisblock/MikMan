87921b7523f64c17e22cebde78b479b6e437e76c
SQL47 <ajidalmajid6@gmail.com>
2020-05-22 01:38:11 +0700

    add template admin

---
+++ app/views/templates/template_admin.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard MikMan</title>
+    <link href="<?= BASEURL; ?>public/fonts/font-awesome/css/all.css" rel="stylesheet">
+    <link href="<?= BASEURL; ?>public/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= BASEURL; ?>public/css/style.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="top_navbar">
+        <div class="list-menu">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="top_menu">
+          <div class="logo">MikMan</div>
+          <div class="logout"><a href="<?= BASEURL ?>dashboard/logout">Logout</a></div>
+        </div>
+      </div>
+      <div class="sidebar">
+        <ul class="list-unstyled components">
+          <li>
+            <a href="<?= BASEURL; ?>admin" class="parent">
+              <span class="icon">
+                <i class="fa fa-tachometer-alt"></i>
+              </span>
+              <span class="title">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle parent">
+              <span class="icon"><i class="fa fa-wifi"></i></span>
+              <span class="title">Hotspot</span>
+            </a>
+            <ul class="collapse list-unstyled submenu" id="pageSubmenu">
+                <li>
+                    <a href="<?= BASEURL; ?>users">
+                      <span class="icon"><i class="fa fa-users"></i></span>
+                      <span class="title">Users</span>
+                    </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon"><i class="fa fa-users-cog"></i></span>
+                    <span class="title">User Profile</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon"><i class="fa fa-wifi"></i></span>
+                    <span class="title">Hotspot</span>
+                  </a>
+                </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#" class="parent">
+              <span class="icon"><i class="fa fa-list"></i></span>
+              <span class="title">Log</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="parent">
+              <span class="icon"><i class="fa fa-cog"></i></span>
+              <span class="title">System</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="container-mikman">
+
+        <!-- CONTENT -->
+        <?php
+          if(!empty($data['content'])){
+            require_once 'app/views/'. $data['content'] .'.php';
+          }
+        ?>
+
+      </div>
+    </div>
+  <script src="<?= BASEURL; ?>public/js/jquery.min.js"></script>
+  <script src="<?= BASEURL; ?>public/js/bootstrap.min.js"></script>
+  <script src="<?= BASEURL; ?>public/js/script.js"></script>
+  </body>
+</html>
