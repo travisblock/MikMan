a8c1d86f14daaa60dec3f4575bfe096b8669ceb2
SQL47 <ajidalmajid6@gmail.com>
2020-05-23 01:29:43 +0700

    Core redirect

--- app/core/Redirect.php
+++ app/core/Redirect.php
@@ -6,9 +6,11 @@
 * @version 0.0.1
 */
 
-class Redirect{
-  public static function to($location = null){
-    if($location){
+class Redirect
+{
+  public static function to($location = null)
+  {
+    if ($location) {
       header('Location:'. BASEURL . $location);
       exit();
     }
