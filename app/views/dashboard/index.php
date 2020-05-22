026c7439588f10b5c45e377f5f27bcada0ef03e3
SQL47 <ajidalmajid6@gmail.com>
2020-05-23 01:30:07 +0700

    view dashboard

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,9 +1,3 @@
-
-      <!-- <div class="content-mikman">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div> -->
-
       <div class="row">
         <div class="col-md-6">
           <div class="col-lg-12 bg-light pb-3">
@@ -34,8 +28,7 @@
         </div>
         <div class="col-md-6">
           <div class="col-lg-12 bg-light">
-            <h6 class="text-center">Setting roter</h6>
-
+            <h6 class="text-center">Setting Admin</h6>
           </div>
         </div>
       </div>
