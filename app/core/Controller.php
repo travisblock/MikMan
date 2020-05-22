d55abfcacefd92ac16d3276dfef0ba607d5329b7
SQL47 <ajidalmajid6@gmail.com>
2020-05-23 01:46:46 +0700

    Core Controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -8,8 +8,8 @@ class Controller
   {
     $this->API = RouterosAPI::getAPI();
     if ($this->isLogin()) {
-        if ($this->API->connect($_SESSION['ip'], $_SESSION['user'], $_SESSION['pass']))
-          or die("Disconnect");
+        if (!$this->API->connect($_SESSION['ip'], $_SESSION['user'], $_SESSION['pass']))
+            die("Disconnect");
     }
 
   }
