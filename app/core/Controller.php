19364c4931f00d0cdced0f33bc3f6a7a78f77ace
SQL47 <ajidalmajid6@gmail.com>
2020-05-23 01:29:19 +0700

    Core controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,42 +1,49 @@
 <?php
 
-class Controller{
+class Controller
+{
+  protected $API = null;
 
-  public $API = null;
-  public $template = null;
-
-  public function __construct(){
+  public function __construct()
+  {
     $this->API = RouterosAPI::getAPI();
-    if($this->is_login()){
-      $this->API->connect($_SESSION['ip'], $_SESSION['user'], $_SESSION['pass']);
+    if ($this->isLogin()) {
+        if ($this->API->connect($_SESSION['ip'], $_SESSION['user'], $_SESSION['pass']))
+          or die("Disconnect");
     }
 
   }
 
-  public function view($view, $data = []){
+  public function view($view, $data = [])
+  {
     require_once 'app/views/'. $view .'.php';
   }
 
-  public function model($model){
+  public function model($model)
+  {
     require_once 'app/models/'. $model .'.php';
     return new $model;
   }
 
-  public function is_login(){
+  public function isLogin()
+  {
     return (Session::exists('MikMan')) ? TRUE : FALSE;
   }
 
-  public function is_login_dashboard(){
+  public function isLoginDashboard()
+  {
     return (Session::exists('LoginAdmin')) ? TRUE : FALSE;
   }
 
-  public function display_dashboard($template, $judul, $data=null){
+  public function displayDashboard($template, $judul, $data=null)
+  {
     $data['judul']   = $judul;
     $data['content'] = $template;
     $this->view("templates/template_dashboard", $data);
   }
 
-  public function display_admin($template, $judul, $data=null){
+  public function displayAdmin($template, $judul, $data=null)
+  {
     $data['judul']   = $judul;
     $data['content'] = $template;
     $this->view("templates/template_admin", $data);
