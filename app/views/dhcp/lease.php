de54563ede50b64b0173aa120cc9e95a655d820b
SQL47 <ajidalmajid6@gmail.com>
2020-05-27 23:16:10 +0700

    add view DHCP Lease

---
+++ app/views/dhcp/lease.php
@@ -0,0 +1,26 @@
+<div class="content-mikman text-center">
+  <h4>DHCP Lease</h4>
+</div>
+
+  <!-- <div class="menu-under-responsive text-center">
+    <span class="badge badge-primary"><a class="normal" href="<?= BASEURL; ?>users">List</a></span>
+    <span class="badge badge-primary"><a class="normal" href="<?= BASEURL; ?>users/add">Add</a></span>
+    <span class="badge badge-primary"><a class="normal" href="<?= BASEURL; ?>users/generate">Generate</a></span>
+  </div> -->
+
+  <div class="content-mikman">
+    <div class="table responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Server</th>
+            <th>Address</th>
+            <th>Mac</th>
+          </tr>
+        </thead>
+        <tbody id="tableData">
+
+        </tbody>
+      </table>
+    </div>
+  </div>
