d2717bb0c81b225121f88f441456e65c4d3b08e5
SQL47 <ajidalmajid6@gmail.com>
2020-06-08 23:26:10 +0700

    update javascript

--- public/js/script.js
+++ public/js/script.js
@@ -48,12 +48,12 @@ $(document).ready(function () {
     });
 
     // Info Dashboard
-    if(window.location.pathname === '/RouterDashboard'){
+    if(window.location.pathname === '/router_dashboard'){
 
       //setInterval(function(){
         $.ajax({
           type: "POST",
-          url: baseurl + '/RouterDashboard/infoDashboard',
+          url: baseurl + '/router_dashboard/infoDashboard',
           success: function(data){
             var obj = jQuery.parseJSON(data);
             $('#date').html(obj['date']);
@@ -71,7 +71,7 @@ $(document).ready(function () {
 
 			$.ajax({
 				type: "POST",
-				url: baseurl + '/RouterDashboard/showLog',
+				url: baseurl + '/router_dashboard/showLog',
 				success: function(log){
 					var log   = jQuery.parseJSON(log);
 					var items = '';
@@ -88,7 +88,7 @@ $(document).ready(function () {
 
     // User List
 
-    if(window.location.pathname === '/HotspotUsers'){
+    if(window.location.pathname === '/hotspot_users'){
 
 			fetch_data();
 
@@ -97,7 +97,7 @@ $(document).ready(function () {
 					"processing" : true,
 					"serverSide" : true,
 					"ordering"	 : false,
-					"ajax"			 : baseurl + "/HotspotUsers/list"
+					"ajax"			 : baseurl + "/hotspot_users/list"
 				});
 			}
 
@@ -105,7 +105,7 @@ $(document).ready(function () {
 				var id = $(this).attr("id");
 				if(confirm("Are you sure you want to remove this?")){
 					$.ajax({
-						url: baseurl+'/HotspotUsers/delete/'+id,
+						url: baseurl+'/hotspot_users/delete/'+id,
 						method: 'GET',
 						success: function(msgSuccess){
 							$('#alert_message').html('<div class="alert alert-success">'+msgSuccess+'</div>');
@@ -116,16 +116,69 @@ $(document).ready(function () {
 				}
 			});
 
+			// list user click
+			$(document).on('click', '#btnlist', function(){
+				$('#listuser').css('display', 'block');
+				$('#adduser').css('display', 'none');
+				$('h4').text('List Hotspot User');
+				$('title').text('List Hotspot User');
+				$(this).removeClass('badge-info');
+				$(this).addClass('badge-primary');
+				//window.history.pushState('', '', '/hotspot_users');
+			});
+
+			// add user click
+			$(document).on('click', '#btnadd', function(){
+				$('#listuser').css('display', 'none');
+				$('#adduser').css('display', 'block');
+				$('h4').text('Add Hotspot User');
+				$('title').text('Add Hotspot User');
+				$(this).removeClass('badge-info');
+				$(this).addClass('badge-primary');
+				//window.history.pushState('', '', '/hotspot_users/add');
+			});
+
+			// add user submit
+			$(document).on('click', '#btnaddsave', function(){
+				$.ajax({
+					url: baseurl+'/hotspot_users/add',
+					type: 'POST',
+					data: $('#formadduser').serialize(),
+					success: function(msgadduser){
+						console.log($('#formadduser').serialize());
+						$('#alert_message').html('<div class="alert alert-success">'+msgadduser+'</div>');
+						$('#listuser').css('display', 'block');
+						$('#adduser').css('display', 'none');
+						$('h4').text('List Hotspot User');
+						$('#tableUser').DataTable().destroy();
+						fetch_data();
+					},
+					error: function(xhr, status, err) {
+
+             $("Terjadi error : "+status);
+           }
+				})
+			});
+
+			// Close add user
+			$(document).on('click', '#btnaddclose', function(){
+				$('#listuser').css('display', 'block');
+				$('#adduser').css('display', 'none');
+				$('h4').text('List Hotspot User');
+			});
+
+
+
 			setInterval(function(){
 				$('#alert_message').html('');
 			}, 5000);
     };
 
 	// DHCP Lease
-	if(window.location.pathname === '/DhcpLease'){
+	if(window.location.pathname === '/dhcp_lease'){
 		$.ajax({
 			type: "POST",
-	        url: baseurl + '/DhcpLease/list',
+	        url: baseurl + '/dhcp_lease/list',
 	        success: function(data){
 				var json = jQuery.parseJSON(data);
 	            var items = '';
