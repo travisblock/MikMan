19867f05ea4cd206ba6fa7b8594335bcf8a8282b
SQL47 <ajidalmajid6@gmail.com>
2020-06-09 22:21:51 +0700

    fix flash message

--- app/core/Msg.php
+++ app/core/Msg.php
@@ -9,7 +9,7 @@
 class Msg
 {
 
-  public static function setMsg($msg, $type)
+  public static function setMsg($type, $msg)
   {
     $_SESSION['msg'] = [
       'msg'  => $msg,
@@ -20,11 +20,24 @@ class Msg
   public static function show()
   {
     if (isset($_SESSION['msg'])) {
-      echo '<div id="msg" class="msg">
-              <div class="'. $_SESSION['msg']['type'] .'">
-                '. $_SESSION['msg']['msg'] .'
-              </div>
-            </div>';
+
+			if ($_SESSION['msg']['type'] === 'success') {
+				echo '<div id="notify">
+								<div class="msg success"><i class="fa fa-check-circle"></i> <b>Success</b><br>'.$_SESSION['msg']['msg'].'<i class="fa fa-times close"></i></div>
+							</div>';
+			} elseif ($_SESSION['msg']['type'] === 'info') {
+				echo '<div id="notify">
+								<div class="msg info"><i class="fa fa-info-circle"></i> <b>Info</b><br>'.$_SESSION['msg']['msg'].'<i class="fa fa-times close"></i></div>
+							</div>';
+			} elseif ($_SESSION['msg']['type'] === 'warning') {
+				echo '<div id="notify">
+								<div class="msg warning"><i class="fa fa-exclamation-circle"></i> <b>Warning</b><br>'.$_SESSION['msg']['msg'].'<i class="fa fa-times close"></i></div>
+							</div>';
+			} else {
+				echo '<div id="notify">
+								<div class="msg error"><i class="fa fa-times-circle"></i> <b>Error</b><br>'.$_SESSION['msg']['msg'].'<i class="fa fa-times close"></i></div>
+							</div>';
+			}
 
       unset($_SESSION['msg']);
     }
