716f44da9f8fc805e1e90827a08b5255a80d4d3c
SQL47 <ajidalmajid6@gmail.com>
2020-06-09 22:23:01 +0700

    add hotspot user profile controller

---
+++ app/controllers/HotspotUserProfile.php
@@ -0,0 +1,43 @@
+<?php
+
+class HotspotUserProfile extends Controller
+{
+		private $kelompok = "hotspot";
+    private $judul    = "Hotspot User Profile";
+    public  $template = "user_profile";
+
+    public function __construct()
+    {
+      parent::__construct();
+      if (!$this->isLogin())
+        Redirect::to('/');
+    }
+
+    public function index()
+    {
+			$data['profile'] = $this->list();
+	    $this->displayAdmin($this->kelompok . '/' . $this->template, $this->judul, $data);
+    }
+
+	public function list(){
+		$list = $this->API->comm('/ip/hotspot/user/profile/print');
+		$profile = null;
+		if (!empty($list)) {
+	    foreach ($list as $key => $value) {
+	      $profile[] = array(
+				'id'			=> $value['.id'],
+				'name'		=> $value['name'],
+				'session'	=> $value['session-timeout'],
+				'idle'		=> $value['idle-timeout'],
+				'shared'	=> $value['shared-users'],
+				'rate'		=> $value['rate-limit'],
+				'action'	=> "<a class='badge badge-primary' href='".BASEURL."hotspot_user_profile/edit/".$value['.id']."'>Edit</a>
+				 							<a class='badge badge-danger delete normal' id='".$value['.id']."'>Delete</a>"
+	      );
+	    }
+
+		}
+
+		return $profile;
+	}
+}
